78dddc0bd7a3cb452a4f384c93d814066db3c700
☈king <rking@sharpsaw.org>
2012-12-05 23:42:31 -0600

    Eigenclass notation

--- lib/pry-de/backtrace_edit.rb
+++ lib/pry-de/backtrace_edit.rb
@@ -1,9 +1,11 @@
 module PryDe
   class BacktraceEdit
-    def self.parse_line input
-      r = Hash[[:file, :line, :method, :error].zip(input.split(?:))]
-      r[:method].sub! /^in `(.+)'$/, '\1'
-      r
+    class << self
+      def parse_line input
+        r = Hash[[:file, :line, :method, :error].zip(input.split(?:))]
+        r[:method].sub! /^in `(.+)'$/, '\1'
+        r
+      end
     end
   end
 end
